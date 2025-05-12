69922f8bdcc0360fed55a055924d71b747947ffb
Oliver Forral <intrepion@gmail.com>
2025-05-12 17:24:11 -0600

    Added routes.

--- angular-crud-app/src/app/app.routes.ts
+++ angular-crud-app/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { IndexComponent } from './project/index/index.component';
+import { ShowComponent } from './project/show/show.component';
+import { CreateComponent } from './project/create/create.component';
+import { EditComponent } from './project/edit/edit.component';
+ 
+export const routes: Routes = [
+    { path: '', redirectTo: 'project/index', pathMatch: 'full'},
+    { path: 'project', redirectTo: 'project/index', pathMatch: 'full'},
+    { path: 'project/index', component: IndexComponent },
+    { path: 'project/:id/show', component: ShowComponent },
+    { path: 'project/create', component: CreateComponent },
+    { path: 'project/:id/edit', component: EditComponent } 
+];
